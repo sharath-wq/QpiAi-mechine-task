4d21ea2bfec21580121fd2496f016859e991a019
Sharath P <chandranpsharath@gmail.com>
2025-11-14 20:47:06 +0530

    cleark auth added

---
+++ components/navigation.tsx
@@ -0,0 +1,28 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
+export const Navigation = () => {
+  return (
+    <nav className="bg-background border-b border-foreground/10">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16 items-center">
+          <div className="shrink-0">
+            <h1 className="text-xl font-semibold text-foreground">QpiAi</h1>
+          </div>
+          <div className="flex items-center gap-4">
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+
+            <SignedOut>
+              <SignInButton mode="modal">
+                <button className="px-3 py-2 text-sm rounded-md bg-primary text-white">
+                  Sign in
+                </button>
+              </SignInButton>
+            </SignedOut>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
